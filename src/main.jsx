73b575a4a2812800ada2c783f773b65f2cf938e6
f-pinkus <faigypinkus@gmail.com>
2025-07-08 13:20:27 -0400

    update favicon and website title

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://mini-capstone-api-uoh0.onrender.com";
+// axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://mini-capstone-api-uoh0.onrender.com";
+
+const API_BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://mini-capstone-api-uoh0.onrender.com";
 
 axios.defaults.withCredentials = true;
 
