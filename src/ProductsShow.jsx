98f22720d3a29fad36ef4c82bcb3fad5f2bd168d
f-pinkus <faigypinkus@gmail.com>
2025-07-07 22:00:56 -0400

    add image to product show

--- src/ProductsShow.jsx
+++ src/ProductsShow.jsx
@@ -3,7 +3,12 @@ export function ProductsShow({ product }) {
     <div>
       <h1>Product information</h1>
       <p>Name: {product.name}</p>
-      <p>Url: {product.primary_image_url}</p>
+      <img
+        src={product.primary_image_url}
+        className="img-fluid rounded mx-auto d-block"
+        style={{ width: "200px", height: "200px", objectFit: "cover" }}
+        alt={product.name}
+      />
       <p>Price: ${product.price}</p>
       <p>Description: {product.description}</p>
     </div>
